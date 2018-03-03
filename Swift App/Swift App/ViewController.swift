c567a845b4a333f99c75e3c90c9b6c172665757a
David Huynh <>
2018-03-03 01:52:51 -0500

    First commit.

---
+++ Swift App/Swift App/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by David Huynh on 2018-03-03.
+//  Copyright © 2018 David Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    var count = 0
+    
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        label.text = "This is dumb as shit."
+        count+=1
+        if(count>10){
+            label.text = "Stop pushing the damn button!"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
